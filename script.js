d73bd43c61d4ff2a05e0970f04f88afc5c2f9c6c
Cristian Neri <cristian.neri@leitha.eu>
2024-12-05 14:49:09 +0100

    feat: hidden form when password is valid

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ document.getElementById('passwordForm').addEventListener('submit', function (eve
     event.preventDefault();
     const passwordInput = document.getElementById('password').value.trim();
     const resultMessage = document.getElementById('resultMessage');
+    const form = document.getElementById('passwordForm');
 
     if (passwordInput.toLowerCase() === 'ketchup') {
         resultMessage.classList.remove('hidden');
+        form.classList.add('hidden');
     } else {
         alert('Password errata! Riprova.');
     }
